0fddc92315754ad82c4af101aa2032ac88a8e2a8
hdurand22 <hdurand22@gmail.com>
2026-02-24 16:35:01 -0800

    basic scraper

---
+++ scraper-js.js
@@ -0,0 +1,20 @@
+// To run this script, copy and paste 'node scraper-js.js' into your terminal
+
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const cheerio = require('cheerio');
+
+(async () => {
+    const url = 'https://www.example.com/';
+    const response = await fetch(url);
+
+    const $ = cheerio.load(await response.text());
+    const title = $('h1').text();
+    const text = $('p').text();
+    const link = $('a').attr('href');
+    console.log(title);
+    console.log(text);
+    console.log(link);
+    console.log($.html());
+
+})();
